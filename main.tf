9b8ecd2794ca58f8d4aac1b6bbc0d8faa977b372
Daniel Allen <dan@aeternagroup.net>
2026-01-11 14:57:12 +0000

    Update to schema version v4.57.0

---
+++ main.tf
@@ -0,0 +1,221 @@
+resource "azurerm_hdinsight_spark_cluster" "hdinsight_spark_clusters" {
+  for_each = var.hdinsight_spark_clusters
+
+  cluster_version               = each.value.cluster_version
+  location                      = each.value.location
+  name                          = each.value.name
+  resource_group_name           = each.value.resource_group_name
+  tier                          = each.value.tier
+  encryption_in_transit_enabled = each.value.encryption_in_transit_enabled
+  tags                          = each.value.tags
+  tls_min_version               = each.value.tls_min_version
+  zones                         = each.value.zones
+
+  component_version {
+    spark = each.value.component_version.spark
+  }
+
+  gateway {
+    password = each.value.gateway.password
+    username = each.value.gateway.username
+  }
+
+  roles {
+    head_node {
+      password = each.value.roles.head_node.password
+      dynamic "script_actions" {
+        for_each = each.value.roles.head_node.script_actions != null ? [each.value.roles.head_node.script_actions] : []
+        content {
+          name       = script_actions.value.name
+          parameters = script_actions.value.parameters
+          uri        = script_actions.value.uri
+        }
+      }
+      ssh_keys           = each.value.roles.head_node.ssh_keys
+      subnet_id          = each.value.roles.head_node.subnet_id
+      username           = each.value.roles.head_node.username
+      virtual_network_id = each.value.roles.head_node.virtual_network_id
+      vm_size            = each.value.roles.head_node.vm_size
+    }
+    worker_node {
+      dynamic "autoscale" {
+        for_each = each.value.roles.worker_node.autoscale != null ? [each.value.roles.worker_node.autoscale] : []
+        content {
+          dynamic "capacity" {
+            for_each = autoscale.value.capacity != null ? [autoscale.value.capacity] : []
+            content {
+              max_instance_count = capacity.value.max_instance_count
+              min_instance_count = capacity.value.min_instance_count
+            }
+          }
+          dynamic "recurrence" {
+            for_each = autoscale.value.recurrence != null ? [autoscale.value.recurrence] : []
+            content {
+              schedule {
+                days                  = recurrence.value.schedule.days
+                target_instance_count = recurrence.value.schedule.target_instance_count
+                time                  = recurrence.value.schedule.time
+              }
+              timezone = recurrence.value.timezone
+            }
+          }
+        }
+      }
+      password = each.value.roles.worker_node.password
+      dynamic "script_actions" {
+        for_each = each.value.roles.worker_node.script_actions != null ? [each.value.roles.worker_node.script_actions] : []
+        content {
+          name       = script_actions.value.name
+          parameters = script_actions.value.parameters
+          uri        = script_actions.value.uri
+        }
+      }
+      ssh_keys              = each.value.roles.worker_node.ssh_keys
+      subnet_id             = each.value.roles.worker_node.subnet_id
+      target_instance_count = each.value.roles.worker_node.target_instance_count
+      username              = each.value.roles.worker_node.username
+      virtual_network_id    = each.value.roles.worker_node.virtual_network_id
+      vm_size               = each.value.roles.worker_node.vm_size
+    }
+    zookeeper_node {
+      password = each.value.roles.zookeeper_node.password
+      dynamic "script_actions" {
+        for_each = each.value.roles.zookeeper_node.script_actions != null ? [each.value.roles.zookeeper_node.script_actions] : []
+        content {
+          name       = script_actions.value.name
+          parameters = script_actions.value.parameters
+          uri        = script_actions.value.uri
+        }
+      }
+      ssh_keys           = each.value.roles.zookeeper_node.ssh_keys
+      subnet_id          = each.value.roles.zookeeper_node.subnet_id
+      username           = each.value.roles.zookeeper_node.username
+      virtual_network_id = each.value.roles.zookeeper_node.virtual_network_id
+      vm_size            = each.value.roles.zookeeper_node.vm_size
+    }
+  }
+
+  dynamic "compute_isolation" {
+    for_each = each.value.compute_isolation != null ? [each.value.compute_isolation] : []
+    content {
+      compute_isolation_enabled = compute_isolation.value.compute_isolation_enabled
+      host_sku                  = compute_isolation.value.host_sku
+    }
+  }
+
+  dynamic "disk_encryption" {
+    for_each = each.value.disk_encryption != null ? [each.value.disk_encryption] : []
+    content {
+      encryption_algorithm          = disk_encryption.value.encryption_algorithm
+      encryption_at_host_enabled    = disk_encryption.value.encryption_at_host_enabled
+      key_vault_key_id              = disk_encryption.value.key_vault_key_id
+      key_vault_managed_identity_id = disk_encryption.value.key_vault_managed_identity_id
+    }
+  }
+
+  dynamic "extension" {
+    for_each = each.value.extension != null ? [each.value.extension] : []
+    content {
+      log_analytics_workspace_id = extension.value.log_analytics_workspace_id
+      primary_key                = extension.value.primary_key
+    }
+  }
+
+  dynamic "metastores" {
+    for_each = each.value.metastores != null ? [each.value.metastores] : []
+    content {
+      dynamic "ambari" {
+        for_each = metastores.value.ambari != null ? [metastores.value.ambari] : []
+        content {
+          database_name = ambari.value.database_name
+          password      = ambari.value.password
+          server        = ambari.value.server
+          username      = ambari.value.username
+        }
+      }
+      dynamic "hive" {
+        for_each = metastores.value.hive != null ? [metastores.value.hive] : []
+        content {
+          database_name = hive.value.database_name
+          password      = hive.value.password
+          server        = hive.value.server
+          username      = hive.value.username
+        }
+      }
+      dynamic "oozie" {
+        for_each = metastores.value.oozie != null ? [metastores.value.oozie] : []
+        content {
+          database_name = oozie.value.database_name
+          password      = oozie.value.password
+          server        = oozie.value.server
+          username      = oozie.value.username
+        }
+      }
+    }
+  }
+
+  dynamic "monitor" {
+    for_each = each.value.monitor != null ? [each.value.monitor] : []
+    content {
+      log_analytics_workspace_id = monitor.value.log_analytics_workspace_id
+      primary_key                = monitor.value.primary_key
+    }
+  }
+
+  dynamic "network" {
+    for_each = each.value.network != null ? [each.value.network] : []
+    content {
+      connection_direction = network.value.connection_direction
+      private_link_enabled = network.value.private_link_enabled
+    }
+  }
+
+  dynamic "private_link_configuration" {
+    for_each = each.value.private_link_configuration != null ? [each.value.private_link_configuration] : []
+    content {
+      group_id = private_link_configuration.value.group_id
+      ip_configuration {
+        name                         = private_link_configuration.value.ip_configuration.name
+        primary                      = private_link_configuration.value.ip_configuration.primary
+        private_ip_address           = private_link_configuration.value.ip_configuration.private_ip_address
+        private_ip_allocation_method = private_link_configuration.value.ip_configuration.private_ip_allocation_method
+        subnet_id                    = private_link_configuration.value.ip_configuration.subnet_id
+      }
+      name = private_link_configuration.value.name
+    }
+  }
+
+  dynamic "security_profile" {
+    for_each = each.value.security_profile != null ? [each.value.security_profile] : []
+    content {
+      aadds_resource_id       = security_profile.value.aadds_resource_id
+      cluster_users_group_dns = security_profile.value.cluster_users_group_dns
+      domain_name             = security_profile.value.domain_name
+      domain_user_password    = security_profile.value.domain_user_password
+      domain_username         = security_profile.value.domain_username
+      ldaps_urls              = security_profile.value.ldaps_urls
+      msi_resource_id         = security_profile.value.msi_resource_id
+    }
+  }
+
+  dynamic "storage_account" {
+    for_each = each.value.storage_account != null ? [each.value.storage_account] : []
+    content {
+      is_default           = storage_account.value.is_default
+      storage_account_key  = storage_account.value.storage_account_key
+      storage_container_id = storage_account.value.storage_container_id
+      storage_resource_id  = storage_account.value.storage_resource_id
+    }
+  }
+
+  dynamic "storage_account_gen2" {
+    for_each = each.value.storage_account_gen2 != null ? [each.value.storage_account_gen2] : []
+    content {
+      filesystem_id                = storage_account_gen2.value.filesystem_id
+      is_default                   = storage_account_gen2.value.is_default
+      managed_identity_resource_id = storage_account_gen2.value.managed_identity_resource_id
+      storage_resource_id          = storage_account_gen2.value.storage_resource_id
+    }
+  }
+}
+
---
+++ variables.tf
@@ -0,0 +1,276 @@
+variable "hdinsight_spark_clusters" {
+  description = <<EOT
+Map of hdinsight_spark_clusters, attributes below
+Required:
+    - cluster_version
+    - location
+    - name
+    - resource_group_name
+    - tier
+    - component_version (block):
+        - spark (required)
+    - gateway (block):
+        - password (required)
+        - username (required)
+    - roles (block):
+        - head_node (required, block):
+            - password (optional)
+            - script_actions (optional, block):
+                - name (required)
+                - parameters (optional)
+                - uri (required)
+            - ssh_keys (optional)
+            - subnet_id (optional)
+            - username (required)
+            - virtual_network_id (optional)
+            - vm_size (required)
+        - worker_node (required, block):
+            - autoscale (optional, block):
+                - capacity (optional, block):
+                    - max_instance_count (required)
+                    - min_instance_count (required)
+                - recurrence (optional, block):
+                    - schedule (required, block):
+                        - days (required)
+                        - target_instance_count (required)
+                        - time (required)
+                    - timezone (required)
+            - password (optional)
+            - script_actions (optional, block):
+                - name (required)
+                - parameters (optional)
+                - uri (required)
+            - ssh_keys (optional)
+            - subnet_id (optional)
+            - target_instance_count (required)
+            - username (required)
+            - virtual_network_id (optional)
+            - vm_size (required)
+        - zookeeper_node (required, block):
+            - password (optional)
+            - script_actions (optional, block):
+                - name (required)
+                - parameters (optional)
+                - uri (required)
+            - ssh_keys (optional)
+            - subnet_id (optional)
+            - username (required)
+            - virtual_network_id (optional)
+            - vm_size (required)
+Optional:
+    - encryption_in_transit_enabled
+    - tags
+    - tls_min_version
+    - zones
+    - compute_isolation (block):
+        - compute_isolation_enabled (optional)
+        - host_sku (optional)
+    - disk_encryption (block):
+        - encryption_algorithm (optional)
+        - encryption_at_host_enabled (optional)
+        - key_vault_key_id (optional)
+        - key_vault_managed_identity_id (optional)
+    - extension (block):
+        - log_analytics_workspace_id (required)
+        - primary_key (required)
+    - metastores (block):
+        - ambari (optional, block):
+            - database_name (required)
+            - password (required)
+            - server (required)
+            - username (required)
+        - hive (optional, block):
+            - database_name (required)
+            - password (required)
+            - server (required)
+            - username (required)
+        - oozie (optional, block):
+            - database_name (required)
+            - password (required)
+            - server (required)
+            - username (required)
+    - monitor (block):
+        - log_analytics_workspace_id (required)
+        - primary_key (required)
+    - network (block):
+        - connection_direction (optional)
+        - private_link_enabled (optional)
+    - private_link_configuration (block):
+        - group_id (required)
+        - ip_configuration (required, block):
+            - name (required)
+            - primary (optional)
+            - private_ip_address (optional)
+            - private_ip_allocation_method (optional)
+            - subnet_id (optional)
+        - name (required)
+    - security_profile (block):
+        - aadds_resource_id (required)
+        - cluster_users_group_dns (optional)
+        - domain_name (required)
+        - domain_user_password (required)
+        - domain_username (required)
+        - ldaps_urls (required)
+        - msi_resource_id (required)
+    - storage_account (block):
+        - is_default (required)
+        - storage_account_key (required)
+        - storage_container_id (required)
+        - storage_resource_id (optional)
+    - storage_account_gen2 (block):
+        - filesystem_id (required)
+        - is_default (required)
+        - managed_identity_resource_id (required)
+        - storage_resource_id (required)
+EOT
+
+  type = map(object({
+    cluster_version               = string
+    location                      = string
+    name                          = string
+    resource_group_name           = string
+    tier                          = string
+    encryption_in_transit_enabled = optional(bool)
+    tags                          = optional(map(string))
+    tls_min_version               = optional(string)
+    zones                         = optional(set(string))
+    component_version = object({
+      spark = string
+    })
+    gateway = object({
+      password = string
+      username = string
+    })
+    roles = object({
+      head_node = object({
+        password = optional(string)
+        script_actions = optional(object({
+          name       = string
+          parameters = optional(string)
+          uri        = string
+        }))
+        ssh_keys           = optional(set(string))
+        subnet_id          = optional(string)
+        username           = string
+        virtual_network_id = optional(string)
+        vm_size            = string
+      })
+      worker_node = object({
+        autoscale = optional(object({
+          capacity = optional(object({
+            max_instance_count = number
+            min_instance_count = number
+          }))
+          recurrence = optional(object({
+            schedule = object({
+              days                  = list(string)
+              target_instance_count = number
+              time                  = string
+            })
+            timezone = string
+          }))
+        }))
+        password = optional(string)
+        script_actions = optional(object({
+          name       = string
+          parameters = optional(string)
+          uri        = string
+        }))
+        ssh_keys              = optional(set(string))
+        subnet_id             = optional(string)
+        target_instance_count = number
+        username              = string
+        virtual_network_id    = optional(string)
+        vm_size               = string
+      })
+      zookeeper_node = object({
+        password = optional(string)
+        script_actions = optional(object({
+          name       = string
+          parameters = optional(string)
+          uri        = string
+        }))
+        ssh_keys           = optional(set(string))
+        subnet_id          = optional(string)
+        username           = string
+        virtual_network_id = optional(string)
+        vm_size            = string
+      })
+    })
+    compute_isolation = optional(object({
+      compute_isolation_enabled = optional(bool, false)
+      host_sku                  = optional(string)
+    }))
+    disk_encryption = optional(object({
+      encryption_algorithm          = optional(string)
+      encryption_at_host_enabled    = optional(bool)
+      key_vault_key_id              = optional(string)
+      key_vault_managed_identity_id = optional(string)
+    }))
+    extension = optional(object({
+      log_analytics_workspace_id = string
+      primary_key                = string
+    }))
+    metastores = optional(object({
+      ambari = optional(object({
+        database_name = string
+        password      = string
+        server        = string
+        username      = string
+      }))
+      hive = optional(object({
+        database_name = string
+        password      = string
+        server        = string
+        username      = string
+      }))
+      oozie = optional(object({
+        database_name = string
+        password      = string
+        server        = string
+        username      = string
+      }))
+    }))
+    monitor = optional(object({
+      log_analytics_workspace_id = string
+      primary_key                = string
+    }))
+    network = optional(object({
+      connection_direction = optional(string, "Inbound")
+      private_link_enabled = optional(bool, false)
+    }))
+    private_link_configuration = optional(object({
+      group_id = string
+      ip_configuration = object({
+        name                         = string
+        primary                      = optional(bool)
+        private_ip_address           = optional(string)
+        private_ip_allocation_method = optional(string)
+        subnet_id                    = optional(string)
+      })
+      name = string
+    }))
+    security_profile = optional(object({
+      aadds_resource_id       = string
+      cluster_users_group_dns = optional(set(string))
+      domain_name             = string
+      domain_user_password    = string
+      domain_username         = string
+      ldaps_urls              = set(string)
+      msi_resource_id         = string
+    }))
+    storage_account = optional(object({
+      is_default           = bool
+      storage_account_key  = string
+      storage_container_id = string
+      storage_resource_id  = optional(string)
+    }))
+    storage_account_gen2 = optional(object({
+      filesystem_id                = string
+      is_default                   = bool
+      managed_identity_resource_id = string
+      storage_resource_id          = string
+    }))
+  }))
+}
+
