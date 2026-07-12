e843db8355727328f324711dd0dd6aa0722d366f
Daniel Allen <dan@aeternagroup.net>
2026-07-12 18:17:18 +0100

    Update to schema version v4.80.0

--- variables.tf
+++ variables.tf
@@ -283,7 +283,7 @@ EOT
   validation {
     condition = alltrue([
       for k, v in var.hdinsight_spark_clusters : (
-        length(v.roles.worker_node.autoscale.recurrence.schedule) >= 1
+        v.roles.worker_node.autoscale == null || (v.roles.worker_node.autoscale.recurrence == null || (length(v.roles.worker_node.autoscale.recurrence.schedule) >= 1))
       )
     ])
     error_message = "Each schedule list must contain at least 1 items"
